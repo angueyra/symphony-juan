f5f05e7ceb1c3a099f2f1f213fded74a6e653680
angueyra <angueyra@gmail.com>
2015-02-01 23:04:07 -0500

    Sinusoidal wave stimuli

---
+++ Protocols/LEDSine/LEDSine.m
@@ -0,0 +1,180 @@
+classdef LEDSine < PulsedProtocol
+    
+    properties (Constant)
+        identifier = 'juan.LEDSine'
+        version = 1
+        displayName = 'LEDSine'
+    end
+    
+    properties
+        led
+        preTime = 100
+        stimTime = 500
+        tailTime = 100
+        
+        freq = 5
+        phase = 0           
+        wcontrast = 25          
+        mean = 2
+        amp
+    end
+    
+    properties (Dependent, SetAccess = private)
+        amp2
+    end
+    
+    properties
+        amp2PulseAmplitude = 0
+        numberOfAverages = uint16(5)
+        interpulseInterval = 0
+    end
+    
+    methods           
+        
+        function p = parameterProperty(obj, parameterName)
+            % Call the base method to create the property object.
+            p = parameterProperty@PulsedProtocol(obj, parameterName);
+            
+            switch parameterName
+                case 'freq'
+                    p.units = 'Hz';
+                case 'led'
+                    p.defaultValue = obj.rigConfig.deviceNames('led');
+                case 'mean'
+                    % Support both calibrated and non-calibrated LEDs.
+                    p.units = char(obj.rigConfig.deviceWithName(obj.led).Background.DisplayUnit);
+                    if p.units == Symphony.Core.Measurement.NORMALIZED
+                        p.units = 'norm. [0-1]';
+                    end
+                case 'phase'
+                    p.units = 'deg';
+                case 'wcontrast'
+                    p.units = '%%';
+                case 'interpulseInterval'
+                    p.units = 's';
+            end
+        end
+        
+        
+        function prepareRun(obj)
+            % Call the base method.
+            prepareRun@PulsedProtocol(obj);
+            
+            % Open figure handlers.
+            if obj.rigConfig.numMultiClampDevices() > 1
+                obj.openFigure('Dual Response', obj.amp, obj.amp2);
+                obj.openFigure('Dual Mean Response', obj.amp, obj.amp2);
+                obj.openFigure('Dual Mean vs. Epoch', obj.amp, obj.amp2, 'EndPt', obj.prePts);
+                obj.openFigure('Dual Variance vs. Epoch', obj.amp, obj.amp2, 'EndPt', obj.prePts);
+            else
+                obj.openFigure('Data', obj.amp);
+%                 obj.openFigure('Stim', obj.amp);
+                obj.openFigure('Mean Response', obj.amp);
+                obj.openFigure('Mean vs. Epoch', obj.amp, 'EndPt', obj.prePts);
+                obj.openFigure('Variance vs. Epoch', obj.amp, 'EndPt', obj.prePts);
+            end
+        end
+        
+        
+        function stim = ampStimulus(obj)
+            % Main amp stimulus.           
+            p = SineGenerator();
+            
+            device = obj.rigConfig.deviceWithName(obj.amp);
+            p.preTime = obj.preTime;
+            p.stimTime = obj.stimTime;
+            p.tailTime = obj.tailTime;
+%             p.mean = double(System.Convert.ToDouble(device.Background.Quantity));
+            p.mean = obj.mean;
+            p.freq = obj.freq;
+            p.phase = obj.phase;
+            p.wcontrast = obj.wcontrast;
+            p.freq = obj.freq;
+            p.sampleRate = obj.sampleRate;
+            p.units = char(device.Background.DisplayUnit);
+            
+            stim = p.generate();
+        end
+        
+        
+        function stim = amp2Stimulus(obj)
+            % Secondary amp stimulus.
+            p = SineGenerator();
+            
+            device = obj.rigConfig.deviceWithName(obj.amp2);
+            p.preTime = obj.preTime;
+            p.stimTime = obj.stimTime;
+            p.tailTime = obj.tailTime;
+%             p.mean = double(System.Convert.ToDouble(device.Background.Quantity));
+            p.mean = obj.mean;
+            p.freq = obj.freq;
+            p.phase = obj.phase;
+            p.wcontrast = obj.wcontrast;
+            p.freq = obj.freq;
+            p.sampleRate = obj.sampleRate;
+            p.units = char(device.Background.DisplayUnit);
+            
+            stim = p.generate();
+        end
+        
+        
+        function stimuli = sampleStimuli(obj)
+            % Return main amp stimulus for display in the edit parameters window.
+            stimuli{1} = obj.ampStimulus();
+        end
+        
+        
+        function prepareEpoch(obj, epoch)            
+            prepareEpoch@PulsedProtocol(obj, epoch);
+            
+            % Add main amp stimulus.
+            epoch.addStimulus(obj.amp, obj.ampStimulus());
+            
+            % Add secondary amp stimulus if the rig config is two amp.
+            if obj.rigConfig.numMultiClampDevices() > 1
+                epoch.addStimulus(obj.amp2, obj.amp2Stimulus());
+            end
+        end
+        
+        
+        function queueEpoch(obj, epoch)            
+            % Call the base method to queue the actual epoch.
+            queueEpoch@PulsedProtocol(obj, epoch);
+            
+            % Queue the inter-pulse interval after queuing the epoch.
+            if obj.interpulseInterval > 0
+                obj.queueInterval(obj.interpulseInterval);
+            end
+        end
+        
+        
+        function keepQueuing = continueQueuing(obj)
+            % Check the base class method to make sure the user hasn't paused or stopped the protocol.
+            keepQueuing = continueQueuing@PulsedProtocol(obj);
+            
+            % Keep queuing until the requested number of averages have been queued.
+            if keepQueuing
+                keepQueuing = obj.numEpochsQueued < obj.numberOfAverages;
+            end
+        end
+        
+        
+        function keepGoing = continueRun(obj)
+            % Check the base class method to make sure the user hasn't paused or stopped the protocol.
+            keepGoing = continueRun@PulsedProtocol(obj);
+            
+            % Keep going until the requested number of averages have been completed.
+            if keepGoing
+                keepGoing = obj.numEpochsCompleted < obj.numberOfAverages;
+            end
+        end
+        
+        
+        function amp2 = get.amp2(obj)
+            amp2 = obj.get_amp2();
+        end
+        
+    end
+    
+end
+
---
+++ Stimulus Generators/SineGenerator.m
@@ -0,0 +1,66 @@
+% Generates a sinusoidal stimulus.
+
+classdef SineGenerator < StimulusGenerator
+    
+    properties (Constant)
+        identifier = 'edu.washington.rieke.GaussianNoiseGenerator'
+        version = 1
+    end
+    
+    properties
+        preTime             % Leading duration (ms)
+        stimTime            % Noise duration (ms)
+        tailTime            % Trailing duration (ms)
+        freq                % Noise frequency cutoff for smoothing (Hz)
+        phase               % Phase of the sinusoid in degrees
+        wcontrast           % Weber contrast of sine modulation
+        mean                % Mean amplitude (units)
+        
+        
+        sampleRate          % Sample rate of generated stimulus (Hz)
+        units               % Units of generated stimulus
+    end
+    
+    methods
+        
+        function obj = SineGenerator(params)
+            if nargin == 0
+                params = struct();
+            end
+            
+            obj = obj@StimulusGenerator(params);
+        end
+        
+    end
+    
+    methods (Access = protected)
+        
+        function stim = generateStimulus(obj)
+            import Symphony.Core.*;
+            
+            timeToPts = @(t)(round(t / 1e3 * obj.sampleRate));
+            
+            prePts = timeToPts(obj.preTime);
+            stimPts = timeToPts(obj.stimTime);
+            tailPts = timeToPts(obj.tailTime);
+            
+            fPts= obj.freq / obj.sampleRate;
+            phaseRad=deg2rad(obj.phase);
+            
+            wcontrast=obj.wcontrast/100;
+            
+            sinewave=sin(2*pi*fPts*[1:stimPts]+phaseRad);
+            sinewave = (sinewave*wcontrast*obj.mean) + obj.mean;
+            
+            data = ones(1, prePts + stimPts + tailPts) * obj.mean;
+            data(prePts + 1:prePts + stimPts) = sinewave;
+            measurements = Measurement.FromArray(data, obj.units);
+            rate = Measurement(obj.sampleRate, 'Hz');
+            output = OutputData(measurements, rate);
+            
+            stim = RenderedStimulus(obj.identifier, obj.stimulusParameters, output);
+        end
+        
+    end
+    
+end
